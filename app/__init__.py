57289a62fb1e83ecc3764e46c23f251ed293b161
evgen <dresvyannikov-eo@otdel332.avt>
2018-01-15 14:21:54 +0500

    Подключил bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object(Config)  # Задание конфигурации приложения
@@ -18,7 +20,7 @@ login = LoginManager(app)
 login.login_view = 'login'  # Указание какой route использовать при login
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+Bootstrap(app)
 
 from app import routes, models
 
